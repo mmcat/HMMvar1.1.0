b937eedfc670892d6a5780d989ffd4e82a5bdd4e
mingming <mingming@leusine.cbb.lan>
2014-12-29 10:15:53 -0500

    add AHC clustering

---
+++ AHCluster.cpp
@@ -0,0 +1,18 @@
+/*
+ * AHCluster.cpp
+ *
+ *  Created on: Dec 28, 2014
+ *      Author: mingming
+ */
+
+#include "AHCluster.h"
+
+AHCluster::AHCluster() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AHCluster::~AHCluster() {
+	// TODO Auto-generated destructor stub
+}
+
